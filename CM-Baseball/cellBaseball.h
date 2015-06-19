ca4cfdb8e4fc497a8a362f7d9d6f18ce403d74ee
Vita V <dj.vita.09@gmail.com>
2015-06-18 21:10:56 -0500

    Add cell

---
+++ CM-Baseball/cellBaseball.h
@@ -0,0 +1,13 @@
+//
+//  cellBaseball.h
+//  CM-Baseball
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 6/18/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellBaseball : UITableViewCell
+
+@end
---
+++ CM-Baseball/cellBaseball.m
@@ -0,0 +1,23 @@
+//
+//  cellBaseball.m
+//  CM-Baseball
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 6/18/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import "cellBaseball.h"
+
+@implementation cellBaseball
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
