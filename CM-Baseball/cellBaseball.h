b719781e062469c42bc219e2ef3d509bac31d7ed
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-06-18 21:12:39 -0500

    Cell added

---
+++ CM-Baseball/cellBaseball.h
@@ -0,0 +1,13 @@
+//
+//  cellBaseball.h
+//  CM-Baseball
+//
+//  Created by Walter Gonzalez Domenzain on 18/06/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellBaseball : UITableViewCell
+
+@end
